819203a147aef6559dd3d9ea13dac2271d797dc4
abumalick <abdullah.hilson@gmail.com>
2020-01-22 12:26:23 +0000

    feat: add Post edit view

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from "react";
-import { Admin } from "react-admin";
+import { Admin, Resource, ListGuesser } from "react-admin";
 import jsonServerProvider from "ra-data-json-server";
+import PostEdit from "./PostEdit";
 
 const dataProvider = jsonServerProvider("http://jsonplaceholder.typicode.com");
-const App = () => <Admin dataProvider={dataProvider} />;
+const App = () => {
+  return (
+    <Admin dataProvider={dataProvider}>
+      <Resource name="posts" list={ListGuesser} edit={PostEdit} />
+      <Resource name="users" list={ListGuesser} />
+    </Admin>
+  );
+};
 
 export default App;
---
+++ src/PostEdit.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import {
+  Edit,
+  SimpleForm,
+  TextInput,
+  ReferenceInput,
+  SelectInput
+} from "react-admin";
+
+const PostEdit = props => (
+  <Edit {...props}>
+    <SimpleForm>
+      <TextInput disabled source="id" />
+      <ReferenceInput source="userId" reference="users">
+        <SelectInput optionText="name" />
+      </ReferenceInput>
+      <TextInput source="title" />
+
+      <TextInput multiline source="body" />
+    </SimpleForm>
+  </Edit>
+);
+export default PostEdit;
